db2234be2de12d49a4a9b14f5f0491021ab7aba1
Greg Pechiro <gregpechiro@yahoo.com>
2016-06-01 18:17:55 -0400

    small update

--- ngin/freeset.go
+++ ngin/freeset.go
@@ -1,21 +1,21 @@
 package ngin
 
-type freeset map[int]bool
+type freeset map[int]struct{}
 
 func (fs freeset) has(k int) bool {
-	return fs[k] == true
+	return fs[k] == struct{}{}
 }
 
 func (fs freeset) add(k int) {
-	if !fs[k] {
-		fs[k] = true
+	if fs[k] != struct{}{} {
+		fs[k] = struct{}{}
 	}
 }
 
 func (fs freeset) get() int {
 	if len(fs) > 0 {
 		for i, _ := range fs {
-			if fs[i] {
+			if fs[i] == struct{}{} {
 				delete(fs, i)
 				return i
 			}
