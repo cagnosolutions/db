0a3b193197e5c3d5bc248e30633c023d8ee27980
scottcagno <scottcagno@gmail.com>
2016-08-18 14:08:15 -0400

    added print method from other tree. trying to clean up/diagnose

--- ngin/btree.go
+++ ngin/btree.go
@@ -7,6 +7,112 @@ import (
 	"unsafe"
 )
 
+/* ##### PRINTER ##### */
+
+var queue *node = nil
+
+// utility function to give the length in edges
+// for the path from any node to the root
+func path_to_root(root, child *node) int {
+	var length int
+	var c *node = child
+	for c != root {
+		c = c.rent
+		length++
+	}
+	return length
+}
+
+// helper function for printing the
+// tree out. (see print_tree)
+func enqueue(new_node *node) {
+	var c *node
+	if queue == nil {
+		queue = new_node
+		queue.next = nil
+	} else {
+		c = queue
+		for c.next != nil {
+			c = c.next
+		}
+		c.next = new_node
+		new_node.next = nil
+	}
+}
+
+// helper function for printing the
+// tree out. (see print_tree)
+func dequeue() *node {
+	var n *node = queue
+	queue = queue.next
+	n.next = nil
+	return n
+}
+
+// prints the bottom row of keys of the tree
+func print_leaves(root *node) {
+	var i int
+	var c *node = root
+	if root == nil {
+		fmt.Printf("Empty tree.\n")
+		return
+	}
+	for !c.isLeaf() {
+		c = (*node)(unsafe.Pointer(c.ptrs[0])
+	}
+	for {
+		for i = 0; i < c.num_keys; i++ {
+			fmt.Printf("%s ", c.keys[i])
+		}
+		if c.ptrs[ORDER-1] != nil {
+			fmt.Printf(" | ")
+			c = (*node)(unsafe.Pointer(c.ptrs[ORDER-1]))
+		} else {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// print tree out
+func print_tree(root *node) {
+	var i, rank, new_rank int
+	if root == nil {
+		fmt.Printf("Empty tree.\n")
+		return
+	}
+	queue = nil
+	enqueue(root)
+	for queue != nil {
+		n := dequeue()
+		if n.rent != nil && n == n.rent.ptrs[0] {
+			new_rank = path_to_root(root, n)
+			if new_rank != rank {
+				rank = new_rank
+				fmt.Printf("\n")
+			}
+		}
+		for i = 0; i < n.num_keys; i++ {
+			fmt.Printf("%s ", n.keys[i])
+		}
+		if !n.isLeaf() {
+			for i = 0; i <= n.num_keys; i++ {
+				enqueue((*node)(unsafe.Pointer(p)))
+			}
+		}
+		fmt.Printf("| ")
+	}
+	fmt.Printf("\n")
+}
+
+func (t *btree) Print() {
+	print_tree(t.root)
+	fmt.Println()
+	print_leaves(t.root)
+}
+
+/* ##### PRINTER ##### */
+
 const M = 8 // (ORDER) 56
 
 var zero []byte = nil
@@ -27,7 +133,7 @@ func NewBTree() *btree {
 type node struct {
 	numk int
 	keys [M - 1][]byte
-	ptrs [M]unsafe.Pointer
+	ptrs [M]unsafe.Pointer // might just have to do away with this, and do an int instead?! why not before??!
 	rent *node
 	leaf struct{}
 	next *node
